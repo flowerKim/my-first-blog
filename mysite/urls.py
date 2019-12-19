921b718be2f89bb119c3c20f3f21ad729d23ba4e
flowerKim <aotlf_@nate.com>
2019-12-19 15:39:00 +0900

    mysite/urls.py error..............

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,10 +17,16 @@ from django.contrib import admin
 from django.urls import path, include
 
 from django.contrib.auth import views
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/login', views.login, name='login'),
-    path('accounts/logout', views.logout, name='logout', kwargs={'next_page' : '/'}),
+    path('login', auth_views.LoginView.as_view(), name='login'),
+    path('logout', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
+
+
+
+
     path('', include('blog.urls')),
 ]
